f5a3b7b32f672c0e9d63746d44c4d6c04ffd4adf
gwiley72 <glennwiley@gmail.com>
2016-02-24 13:15:41 -0600

    beginning of night porter app

---
+++ night-porter/night-porter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  night-porter
+//
+//  Created by Glenn Wiley on 2/24/16.
+//  Copyright © 2016 Glenn Wiley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
