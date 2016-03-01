193db0b4c52a4843b601e3f0a15e69db54f6ab3a
gwiley72 <glennwiley@gmail.com>
2016-03-01 08:16:18 -0600

    finished optionals

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -1,9 +1,22 @@
+var regularInt: Int
+var optionalInt: Int?
 
-var firstName: String
-var lastName: String
-var employeeNumber: Int
-// etc.
-var middleName: String?
-var passportNumber: Int?
-var phoneExtension: Int?
+regularInt = 100
+optionalInt = 100
 
+optionalInt = nil
+
+regularInt += 5
+
+// different patterns for unwrapping an optional
+// forced unwrapping
+if optionalInt != nil {
+    var unwrappedInt = optionalInt!
+}
+
+// optional binding
+if let unwrappedInt = optionalInt {
+    print(unwrappedInt)
+} else {
+    
+}
