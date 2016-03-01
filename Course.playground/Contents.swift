b8ec283372a88b166f99dcc6590a0bd9cdf9d46b
gwiley72 <glennwiley@gmail.com>
2016-03-01 08:29:41 -0600

    finished classes tutorial

--- Course.playground/Contents.swift
+++ Course.playground/Contents.swift
@@ -1,9 +1,27 @@
-func createMessage(someNumber: Int, secondNumber: Int) -> String {
-    print("You passed in the number: \(someNumber)")
-    let message = "Simplicity is the ultimate sophistication."
-    print(message)
-    return message
+class Appliance {
+    var manufacturer: String
+    var model: String
+    var voltage: Int
+    var capacity: Int?
+    
+    init() {
+        self.manufacturer = "default manufacturer"
+        self.model = "default model"
+        self.voltage = 120
+    }
+    
+    func getDetails() -> String {
+        var message = "This is the \(self.model) from \(self.manufacturer)."
+        if self.voltage >= 220 {
+            message += "This model is for European usage."
+        }
+        return message
+    }
 }
 
-let result = createMessage(99, secondNumber: 100)
-
+var kettle = Appliance()
+kettle.manufacturer = "megappliance, inc."
+kettle.model = "TeaMaster 5000"
+kettle.voltage = 220
+let details = kettle.getDetails()
+print(details)
