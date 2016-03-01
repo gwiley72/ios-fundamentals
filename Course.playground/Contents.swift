05bd317b2ebf050aebcb49172130f1f9fe7a8da7
gwiley72 <glennwiley@gmail.com>
2016-03-01 08:23:42 -0600

    added playground for other lessons
    finished functions tutorial

---
+++ Course.playground/Contents.swift
@@ -0,0 +1,9 @@
+func createMessage(someNumber: Int, secondNumber: Int) -> String {
+    print("You passed in the number: \(someNumber)")
+    let message = "Simplicity is the ultimate sophistication."
+    print(message)
+    return message
+}
+
+let result = createMessage(99, secondNumber: 100)
+
