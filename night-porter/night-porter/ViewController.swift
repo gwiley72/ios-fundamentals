281a49e72dd56ba184138b98410a82c91b8f7c43
gwiley72 <glennwiley@gmail.com>
2016-03-01 09:25:02 -0600

    finish of building single page application module

--- night-porter/night-porter/ViewController.swift
+++ night-porter/night-porter/ViewController.swift
@@ -9,21 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func changeBackground(sender: AnyObject) {
-        view.backgroundColor = UIColor.darkGrayColor()
-        
-        // get all subviews inside the top-level view
-        let allSubviews = view.subviews
-        
-        for eachView in allSubviews {
-
-            if eachView is UILabel {
-                let myLabel = eachView as! UILabel
-                myLabel.textColor = UIColor.lightGrayColor()
-            }
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +20,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeBackground(sender: AnyObject) {
+        view.backgroundColor = UIColor.darkGrayColor()
+        
+        // get all subviews inside the top-level view
+        let allSubviews = view.subviews
+        
+        for eachView in allSubviews {
+            
+            if eachView is UILabel {
+                let myLabel = eachView as! UILabel
+                myLabel.textColor = UIColor.lightGrayColor()
+            }
+        }
+    }
 
 }
 
