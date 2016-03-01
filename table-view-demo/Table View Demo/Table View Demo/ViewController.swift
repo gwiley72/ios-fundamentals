07a7f87fbc9c1b353981cf57750da908367b0890
gwiley72 <glennwiley@gmail.com>
2016-03-01 09:49:07 -0600

    completed table view demo

--- table-view-demo/Table View Demo/Table View Demo/ViewController.swift
+++ table-view-demo/Table View Demo/Table View Demo/ViewController.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = "This is row \(indexPath.row)"
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
